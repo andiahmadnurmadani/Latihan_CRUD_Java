ab9bc0c47dcbb9d7a524eb9156910436032353d6
Andi Ahmad Nurmadani <andiahmadnurmadani15@gmail.com>
2025-04-07 07:38:24 +0700

    menambahkan fitur update dan delete data mahasiswa

--- src/App.java
+++ src/App.java
@@ -2,9 +2,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    String nim;
-    String nama;
-    String jurusan;
+    private String nim;
+    private String nama;
+    private String jurusan;
 
     ArrayList<App> list = new ArrayList<>();
     static Scanner input = new Scanner(System.in);
@@ -12,6 +12,18 @@ public class App {
     App(String nim, String nama, String jurusan) {
         this.nim = nim;
         this.nama = nama;
+        this.jurusan = jurusan;
+    }
+
+    void setnim(String nim) {
+        this.nim = nim;
+    }
+
+    void setnama(String nama) {
+        this.nama = nama;
+    }
+
+    void setjurusan(String jurusan) {
         this.jurusan = jurusan;
     }
 
@@ -37,13 +49,13 @@ public class App {
 
         list.add(new App(nim, nama, jurusan));
 
-        System.out.println("Data berhasil di tambahkan!");
+        System.out.println("== Data berhasil di tambahkan! ==");
     }
 
     void tampilkan() {
         System.out.println("1. Cari berdasarkan nim\n2. Tampilkan semua");
         String pilihTampil = input.next();
-        if (pilihTampil.equalsIgnoreCase("1")){
+        if (pilihTampil.equalsIgnoreCase("1")) {
             System.out.print("Masukkan nim :");
             String cariNim = input.next();
 
@@ -57,16 +69,80 @@ public class App {
                 }
             }
             if (true) {
-                System.out.println("Data tidak ditemukan!");
+                System.out.println("== Data tidak ditemukan! ==");
             }
         } else if (pilihTampil.equalsIgnoreCase("2")) {
-            System.out.println("===== Data Ditemukan! =====");
-            for (App b : list) {
-                System.out.println("\nNIM : " + b.nim + "\nNama : " + b.nama + "\nJurusan : " + b.jurusan + "\n \n");
+            if (list.isEmpty()) {
+                System.out.println("== Data kosong! ==");
+            } else {
+                System.out.println("===== Data Ditemukan! =====");
+                for (App b : list) {
+                    System.out.println("\nNIM : " + b.nim + "\nNama : " + b.nama + "\nJurusan : " + b.jurusan + "\n \n");
+                }
             }
-            System.out.println("===== Data Ditemukan! =====");
+
         } else {
-            System.out.println("Masukkan input dengan benar!");
+            System.out.println("== Masukkan input dengan benar! ==");
+        }
+    }
+
+    void ubah() {
+        System.out.print("Masukkan nim : ");
+        String carinim = input.next();
+        boolean validasi = false;
+        for (App ubah : list) {
+            if (carinim.equalsIgnoreCase(ubah.nim)) {
+
+                input.nextLine();
+
+                System.out.print("Masukkan nim baru : ");
+                String nimBaru = input.nextLine();
+
+                System.out.print("Masukkan nama baru : ");
+                String namaBaru = input.nextLine();
+
+                System.out.print("Masukkan jurusan baru : ");
+                String jurusanBaru = input.nextLine();
+
+                ubah.setnim(nimBaru);
+                ubah.setnama(namaBaru);
+                ubah.setjurusan(jurusanBaru);
+
+                System.out.println("== Data berhasil di ubah ==");
+                validasi = true;
+            }
+        }
+        if (!validasi) {
+            System.out.println("== Data tidak ditemukan ==");
+        }
+    }
+
+    void hapus() {
+        System.out.print("Masukkan nim :");
+        String cariNim = input.next();
+
+        boolean validasi = false;
+        for (int i = 0; i < list.size(); i++) {
+            if (cariNim.equals(list.get(i).nim)) {
+                System.out.println("\nNim : " + list.get(i).nim + "\nNama : " + list.get(i).nama + "\nJurusan : "
+                        + list.get(i).jurusan + "\n");
+                System.out.print("Hapus (Y/N): ");
+                String pilihan = input.next();
+
+                if (pilihan.equalsIgnoreCase("y")) {
+                    list.remove(i);
+                } else if (pilihan.equalsIgnoreCase("n")) {
+                    break;
+                } else {
+                    System.out.println("== Masukkan input dengan benar! ==");
+                }
+
+                validasi = true;
+                System.out.println("== Data dihapus! ==");
+            }
+        }
+        if (!validasi) {
+            System.out.println("== Data tidak ditemukan ==");
         }
     }
 
@@ -83,6 +159,14 @@ public class App {
                 case 2:
                     crud.tampilkan();
                     break;
+                case 3:
+                    crud.ubah();
+                    break;
+                case 4:
+                    crud.hapus();
+                    break;
+                case 5:
+                    return;
 
                 default:
                     break;
